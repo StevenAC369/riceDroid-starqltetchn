907fc46c0c99bfcb359725b18dbccec850e83e4b
StevenAC369 <84295861+StevenAC369@users.noreply.github.com>
2022-12-15 23:07:39 -0500

    Create build_rom.sh

---
+++ build_rom.sh
@@ -0,0 +1,12 @@
+repo init --depth=1 --no-repo-verify -u https://github.com/RiceDroid/android -b thirteen -g default,-mips,-darwin,-notdefault
+git clone https://github.com/Sasukezin678/Android_device_samsung_starqltechn --depth 1 -b master .repo/local_manifests
+repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
+
+# build rom
+source build/envsetup.sh
+lunch derp_startqltechn-user
+export TZ=Europe/Spanish #put before last build command
+mka derp
+
+# upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
+rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
